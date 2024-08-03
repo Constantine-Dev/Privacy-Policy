9615139147b2a96a9df5192eaeac012c63149d1f
Constantine Dev <constantinedev@protonmail.com>
2024-08-03 03:25:45 +0100

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -1,4 +1,5 @@
 import re, io, os, sys, ssl, ast, json, csv, requests, logging
 from datetime import datetime as DT, timezone as TZ
 
-async def example(event, slefClinet):
+async def ReadMe():
+  print('policy.md')
